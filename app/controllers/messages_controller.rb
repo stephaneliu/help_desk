0b43ab2b95c3e50446f001d52e5113155a1682a9
Stephane Liu <sliu@sjliu.com>
2021-03-03 21:57:05 -1000

    Feat: Turbostream new messages
      - New messages created on room show page are appended to dom by
        Turbostream

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,10 @@ class MessagesController < ApplicationController
   def create
     @message = @room.messages.create!(message_params)
 
-    respond_to { |format| format.html { redirect_to @room } }
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @room }
+    end
   end
 
   private
